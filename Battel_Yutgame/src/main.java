23ac252c7e19bd12f08e64b35d7280be94d24e0a
humblebe <hobin.park90@gmail.com>
2015-09-06 13:09:57 +0900

    바꿉시다

--- Battel_Yutgame/src/main.java
+++ Battel_Yutgame/src/main.java
@@ -17,48 +17,13 @@ public class main {
 		
 		
 		for(int i=0;i<4;i++){
-	
-		mp[i]= new Player();
-		System.out.println("Type your name Player " + i);
-		mp[i].name=sc.next();
+			mp[i]= new Player(blue,white);
+			if(mp[i].getTeam().equals("blue")) blue++;
+			else if(mp[i].getTeam().equals("white")) ++white;
+		System.out.println("From now on, your name is " + mp[i].getName() + ", Player " + i);
+		System.out.println("Now your on " + mp[i].getTeam() + " team, Player " + i);
+		}//end of for loop
 		
-		while(!("blue".equals(mp[i])) && !("white".equals(mp[i]))){
-			
-		mp[i].selectTeam();
-		if(blue==2){
-			mp[i].team="white";
-			System.out.println("your team is white, you don't have any choice");
-		}
-		
-		if(white==2){
-			mp[i].team="blue";
-			System.out.println("your team is blue, you don't have any choice");
-		}
-		
-		if(mp[i].team.equals("blue")){
-			mp[i].team = "blue";
-			blue++;
-		}
-		
-		else if(mp[i].team.equals("white")){
-			mp[i].team = "white";
-			white++;
-		}
-		
-		else System.out.println("You've got wrong team!!");
-		
-		System.out.println(blue + " " + white);
-		}//end of while loop
-		
-		System.out.println("From now on, your name is " + mp[i].name + ", Player " + i);
-		System.out.println("Now your on " + mp[i].team + " team, Player " + i);
-		
-		}
-		
-		System.out.println(mp[0].name + mp[0].team);
-		System.out.println(mp[3].name + mp[3].team);
-		System.out.println(mp[2].name + mp[2].team);
-		System.out.println(mp[1].name + mp[1].team);
 	}
 
 }
