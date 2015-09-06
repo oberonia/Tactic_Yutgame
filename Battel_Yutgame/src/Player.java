7a7f8f63586a87e002ea027a7c987d9379717cb4
humblebe <hobin.park90@gmail.com>
2015-09-06 12:10:23 +0900

    let's change a bit

--- Battel_Yutgame/src/Player.java
+++ Battel_Yutgame/src/Player.java
@@ -1,69 +1,26 @@
-// package boardgame;
+
 import java.util.Scanner;
 
 public class Player{
-	public Player(Scanner sc){
-		this.sc = sc;
-	}
+	//교미world
 	//player's basic variables
-	Scanner sc;
-	int piece,yoot,score,id;
-	int blue = 0;
-	int white = 0;
-	String a,w, name,team;;
-
+	Scanner sc = new Scanner(System.in);
+	int score;
+	String name,team;
+	
 	//추가로 들어갈 것 : position of piece(make array which saves location data), whole board x 3
-
+		
 	void getName(){
-		System.out.println("Type your name, Player");
-		name = sc.next();
-
-		System.out.println("Your name is " + name);
+			System.out.println("Type your name, Player");
+			name = sc.next();
+			System.out.println("Your name is " + name);
 	}//end of getName
 
 	void selectTeam(){
-
-		boolean out;
-		while(out=true){
-			if(blue==2){
-				team="white";
-				System.out.println("your team is white, you don't have any choice");
-				return;
-			}
-
-			if(white==2){
-				team="blue";
-				System.out.println("your team is blue, you don't have any choice");
-				return;
-			}
-
-			System.out.println("choose your team, Player (blue or white)");
-			team = sc.next();
-
-			team = team.toLowerCase();
-
-			if(team.equals("blue")){
-				blue++;
-				return;
-			}
-
-			else if(team.equals("white")){
-				white++;
-				return;
-			}
-
-			else System.out.println("You've got wrong team!!");
-		}
-
+		System.out.println("choose your team, Player (blue or white)");
+		team = sc.next();
+		team = team.toLowerCase();
 	}
 
-	void setPlayer(String n, String t, int p, int y, int s, int i){
-		name = n;
-		team = t;
-		piece = p;
-		yoot = y;
-		score = s;
-		id = i;
-	}//end of setPlayer
 
 }
--- Battel_Yutgame/src/main.java
+++ Battel_Yutgame/src/main.java
@@ -1,41 +1,64 @@
-// package boardgame;
+
 import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//sc.next();
-
+		int blue = 0; // temporary variable for team selection
+		int white = 0; // temporary variable for team selection
+		
+		//hello world
+		
 		System.out.println("Hello players, Glad to see you all");
-		System.out.println("From now on, we will play tactic yootnolee");
+		System.out.println("From now on, we will play battle yootnolee");
 		
-		Player mp = new Player(sc);
-		ThrowingYut throwYut = new ThrowingYut();
-
-		for(int i=1;i<5;i++){
-			System.out.println("Type your name Player " + i);
-			
-			mp.name=sc.next();
-			
-			System.out.println("From now on, your name is " + mp.name + " Player " + i);
-			
-			mp.selectTeam();
+		Player mp[] = new Player[4];
 		
-			mp.setPlayer(mp.name, mp.team, 2, 1, 0, i);
-			
-			System.out.println("배틀 윷놀이는 플레이어 4명이 각자 윷을 하나씩 던져서 그 합계로 서로의 전진할 칸수를 결정합니다.");
-			System.out.println("플레이어"+i+"님의 전진할 칸수를 정합니다.");
-			System.out.println("앞면은 0을, 뒷면은 1을 입력하면 낼 수 있습니다.");
 		
-			throwYut.throwing(i);
+		for(int i=0;i<4;i++){
+	
+		mp[i]= new Player();
+		System.out.println("Type your name Player " + i);
+		mp[i].name=sc.next();
+		
+		while(!("blue".equals(mp[i])) && !("white".equals(mp[i]))){
 			
-			System.out.println("helloworld");
+		mp[i].selectTeam();
+		if(blue==2){
+			mp[i].team="white";
+			System.out.println("your team is white, you don't have any choice");
+		}
+		
+		if(white==2){
+			mp[i].team="blue";
+			System.out.println("your team is blue, you don't have any choice");
+		}
+		
+		if(mp[i].team.equals("blue")){
+			mp[i].team = "blue";
+			blue++;
+		}
+		
+		else if(mp[i].team.equals("white")){
+			mp[i].team = "white";
+			white++;
+		}
+		
+		else System.out.println("You've got wrong team!!");
+		
+		System.out.println(blue + " " + white);
+		}//end of while loop
+		
+		System.out.println("From now on, your name is " + mp[i].name + ", Player " + i);
+		System.out.println("Now your on " + mp[i].team + " team, Player " + i);
 		
 		}
 		
-		sc.close();
-
+		System.out.println(mp[0].name + mp[0].team);
+		System.out.println(mp[3].name + mp[3].team);
+		System.out.println(mp[2].name + mp[2].team);
+		System.out.println(mp[1].name + mp[1].team);
 	}
 
 }
