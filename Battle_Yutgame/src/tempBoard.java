73f908e7fd043dbc96a60403c47b94cd85b0c534
oberon <over0n91@gmail.com>
2015-09-19 04:03:19 +0900

    벡터값 변경, 콘솔에서 윷판을 볼 수 있게 함.

--- Battle_Yutgame/src/tempBoard.java
+++ Battle_Yutgame/src/tempBoard.java
@@ -2,7 +2,30 @@ import java.util.Enumeration;
 import java.util.Vector;
 
 public class tempBoard {
-
+	public tempBoard() {
+		boardReset();
+	}
+	
+	String boardPaper[][] = new String[7][7]; // 윷판에 그림을 찍을 종이
+	
+	private void boardReset() {
+		for(int i=0;i<boardPaper.length;i++){
+			for(int j=0;j<boardPaper[i].length;j++){
+				boardPaper[i][j] = ""; // default value is space
+			}
+		}
+	}
+	
+	public void boardDisplay(){
+		// show the board at console
+		for(int i=0;i<boardPaper.length;i++){
+			for(int j=0;j<boardPaper[i].length;j++){
+				System.out.print(boardPaper[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+	
 	public void Print(Vector<Integer> v) {
 		Enumeration<Integer> e = v.elements();
 		while(e.hasMoreElements()) {
@@ -17,9 +40,9 @@ public class tempBoard {
 
 	@SuppressWarnings("unchecked")
 	public void vectorGenerate() {
-		int v1[] = {23,19,14,10,6,5,4,3,2,1,7,11,16,20,24,25,26,27,28,29}; //add가 귀찮은 배열 1 
-		int v2[] = {6,9,13,15,17,21,24}; //add가 귀찮은 배열 2
-		int v3[] = {1,8,12,15,18,22,29}; //add가 귀찮은 배열 3
+		int v1[] = {42,35,21,14,7,6,5,3,2,1,8,15,29,36,43,44,45,47,48,49}; //add가 귀찮은 배열 1 
+		int v2[] = {7,13,19,25,31,37,43}; //add가 귀찮은 배열 2
+		int v3[] = {1,9,17,25,33,41,49}; //add가 귀찮은 배열 3
 		Vector<Integer> v[] = new Vector[3];
 		v[0] = new Vector<Integer>(20); //20개, 우하단부터 시작하는 테두리 윷판, 검색 2순위
 		v[1] = new Vector<Integer>(7); //7개, 대각선 방향(↙), 검색 1순위
@@ -32,7 +55,7 @@ public class tempBoard {
 		for (int i=0;i<7;i++)
 			v[2].add(v3[i]);
 		
-		String boardDisplay[][] = new String[7][7]; // 윷판에 그림을 찍을 종이
+		
 				
 		for(int i=0;i<3;i++) {
 			Enumeration<Integer> e = v[i].elements();
@@ -44,7 +67,7 @@ public class tempBoard {
 					System.out.print(temp);
 				int a = (temp-1)/7;
 				int b = (temp-1)%7;
-				boardDisplay[a][b] = "O"; // 말이 다닐 수 있는 곳은 O로 표시할 수 있도록 함
+				boardPaper[a][b] = "O"; // 말이 다닐 수 있는 곳은 O로 표시할 수 있도록 함
 				// O이 표시된 곳에 말이 들어가면 특수문자로 표현해야 한다
 				// 0) add가 귀찮은 곳의 배열에 해당하는 원소들을 1~49 기준 표현으로 바꿔넣는다
 				// 1) movNext로 값이 바뀐 다음, 이전 위치에 해당하는 boardDisplay는 O으로 바꾸고,
@@ -58,7 +81,7 @@ public class tempBoard {
 	public static void main(String[] args) {
 		tempBoard tboard = new tempBoard();
 		tboard.vectorGenerate();
-
+		tboard.boardDisplay();
 	}
 
 }
