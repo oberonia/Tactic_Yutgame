8a226b8d0afc0aa64b7cf8e6075242c7f7a218e2
ayCODESabtu <larche0090@gmail.com>
2015-11-22 18:39:30 +0900

    뭔가 고쳤다.
    커밋은 제때 합시다...

--- Battle_Yutgame/src/sw_SelectMove.java
+++ Battle_Yutgame/src/sw_SelectMove.java
@@ -4,53 +4,50 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class sw_SelectMove extends JFrame {
 	//JLabel 
-	JRadioButton 도 = new JRadioButton();
-	JRadioButton 개 = new JRadioButton();
-	JRadioButton 걸 = new JRadioButton();
-	JRadioButton 윷 = new JRadioButton();
-	JRadioButton 모 = new JRadioButton();
+	JRadioButton mv[] = new JRadioButton[5];
+	{
+		for(int i=0;i<mv.length;i++) {
+			mv[i] = new JRadioButton();
+		}
+	}
 	JButton confirm = new JButton("확인");
-	
-	
-	
+	//int a= Player.mv[0]; 플레이어가 가지고 있는 Move를 저장하는 변수
+
+
 	public sw_SelectMove(String title) {
 		super(title);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		Container c = getContentPane();
 		c.setLayout(new BorderLayout());
 		JPanel pRadio = new JPanel();
 		JPanel pButton = new JPanel();
 		ButtonGroup gr = new ButtonGroup();
-		gr.add(도);
-		gr.add(개);
-		gr.add(걸);
-		gr.add(윷);
-		gr.add(모);
+		for(JRadioButton i : mv) {
+			gr.add(i);
+		}
 		pRadio.setLayout(new GridLayout(1,5));
 		pRadio.setLocation(20, 70);
-		pRadio.add(도);
-		pRadio.add(개);
-		pRadio.add(걸);
-		pRadio.add(윷);
-		pRadio.add(모);
+		for(JRadioButton i : mv) {
+			pRadio.add(i);
+		}
 		c.add(pRadio,BorderLayout.CENTER);
-		
+
 		confirm.setSize(60, 20);
 		confirm.setLocation(170, 140);
 		pButton.add(confirm);
 		c.add(pButton,BorderLayout.SOUTH);
-		
+
 		//도.setLocation(HEIGHT, WIDTH);
-		
-		
-		
+
+
+
 		setSize(400,200);
 		setVisible(true);
 		setResizable(false);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(screenSize.width/2-getWidth()/2, screenSize.height/2-getHeight()/2);
-		
+
 	}
 	public static void main(String[] args) {
 		new sw_SelectMove("이동할 칸 수를 고르세요.");
