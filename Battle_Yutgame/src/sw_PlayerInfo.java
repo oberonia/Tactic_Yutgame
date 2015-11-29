e07fc42ef8a8aef2a85a2bb5b496269540265e98
oberonia <over0n91@gmail.com>
2015-11-29 19:44:09 +0900

    MOD: sw_PlayerInfo can show all player's information like scoreboard method.

--- Battle_Yutgame/src/sw_PlayerInfo.java
+++ Battle_Yutgame/src/sw_PlayerInfo.java
@@ -2,7 +2,6 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -17,30 +16,40 @@ public class sw_PlayerInfo extends JFrame{
 		cPane.setLayout(new GridLayout(9,5));
 		
 		cPane.add(new JLabel("Team"));
-		for(int i =0; i<mp.length; i++){
-			//String t1 = Player.getTeamName1();
-			//String t2 = Player.getTeamName2();
-			//cPane.add(new JLabel(t1));
-			//cPane.add(new JLabel(t2));
-
-			String team = mp[i].team;
-			String name = mp[i].name;
-			String malIcon = mp[i].malIcon;
-			int mal1Location = mp[i].mal1;
-			int mal2Location = mp[i].mal2;
-			
-			cPane.add(new JLabel(team));
-			cPane.add(new JLabel(name));
-			cPane.add(new JLabel(malIcon));
-
-		}
 		cPane.add(new JLabel("Player Name"));
 		cPane.add(new JLabel("Mal Icon"));
 		cPane.add(new JLabel("Mal Number"));
 		cPane.add(new JLabel("Lacation"));
-
 		
-		setSize(500, 900);
+		for(int i =0; i<mp.length; i++){
+
+			String team = mp[i].team;
+			String name = mp[i].name;
+			String malIcon = mp[i].malIcon;
+			String mal1Location = Integer.toString(mp[i].mal1); // JLabel은 String 타입만 취급함 
+			String mal2Lacation = Integer.toString(mp[i].mal2);
+			
+			cPane.add(new JLabel(team));
+			cPane.add(new JLabel(name));
+			cPane.add(new JLabel(malIcon));
+			cPane.add(new JLabel("Mal1"));
+			cPane.add(new JLabel(mal1Location));
+			cPane.add(new JLabel(team));
+			cPane.add(new JLabel(name));
+			cPane.add(new JLabel(malIcon));
+			cPane.add(new JLabel("Mal2"));
+			cPane.add(new JLabel(mal2Lacation));
+
+		}
+		
+		// 만약 플레이어가 4명이 아닌 경우, 나머지 칸은 모두 빈칸으로 표시 
+		for(int j = mp.length;j<4;j++){
+			for(int x = 0;x<10;x++){
+				cPane.add(new JLabel(""));
+			}
+		}
+
+		setSize(450, 600);
 		setVisible(true);
 	}
 
