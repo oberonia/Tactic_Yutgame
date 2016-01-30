da91354ddd085818324f9db1caaa8473121e77c9
oberonia <over0n91@gmail.com>
2016-01-30 13:52:43 +0900

    Mod: Move 2 decision buttons to right side

--- Battle_Yutgame/src/sw_YutInfo.java
+++ Battle_Yutgame/src/sw_YutInfo.java
@@ -12,18 +12,15 @@ public class sw_YutInfo extends JFrame{
 		
 		// center 에는 윷 던진 결과 이미지를 넣는다. 
 		
-		// wsest 에는 던질 윷 앞/뒤 선택 버튼 
-		JPanel frontBack = new JPanel();
-		frontBack.setLayout(new GridLayout(1, 2));
+		// east 에는 던질 윷 앞/뒤 선택 버튼 
+		JPanel frontback = new JPanel();	// 앞,뒤 버튼이 들어갈 자리를 만든다. 
+		frontback.setLayout(new BorderLayout(4, 20));
 		JButton front = new JButton("Front");
 		JButton back = new JButton("Back");
-		front.setPreferredSize(getMinimumSize());	// 어쩐지 아무일도 하지 않는 코드1
-		back.setSize(getWidth(), 200);	// 어쩐지 아무일도 하지 않는 코드2
+		frontback.add(front, BorderLayout.WEST);
+		frontback.add(back, BorderLayout.EAST);
+		contentPane.add(frontback, BorderLayout.EAST); // 버튼 2개가 들어간 frontback을 east 위치에 추가한다. 
 
-		frontBack.add(front);
-		frontBack.add(back);
-		
-		contentPane.add(frontBack, BorderLayout.WEST);
 		
 		// south 에는 현재 윷 스택(도개걸윷모 각각)을 보여준다. 
 		JPanel yutstack = new JPanel();
