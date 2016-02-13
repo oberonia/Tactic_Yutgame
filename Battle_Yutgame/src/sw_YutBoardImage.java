e3aa2aff19ca7f654bbce0942a94a2fae6830750
oberonia <over0n91@gmail.com>
2016-02-13 13:16:39 +0900

    어쩐지 레이아웃 안에 레이아웃이 있고, 그 안에 또 레이아웃이...

--- Battle_Yutgame/src/sw_YutBoardImage.java
+++ Battle_Yutgame/src/sw_YutBoardImage.java
@@ -1,30 +1,35 @@
-import java.awt.Container;
+import java.awt.*;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class sw_YutBoardImage extends JFrame {
-	public sw_YutBoardImage(String title) {
-		super(title);
+	public sw_YutBoardImage(JPanel c) {
+	//	super(title);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500,500);
-		setResizable(false);
+		c.setLayout(new BorderLayout());
+		
+		c.setSize(500,500);
+		//setResizable(false);
 		
 		ImageIcon image = new ImageIcon("image/YutBoard.jpg");
 		JLabel bg = new JLabel(image);
-		Container c = getContentPane();
+		// Container c = getContentPane();
 		c.add(bg);
-		setVisible(true);
+//		setVisible(true);
 		
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(screenSize.width/2-getWidth()/2, screenSize.height/2-getHeight()/2);
 	}
+	/*
 	public static void main(String[] args) {
 		new sw_YutBoardImage("윷판");
 	}
+	*/
 
 }
--- Battle_Yutgame/src/sw_YutInfo.java
+++ Battle_Yutgame/src/sw_YutInfo.java
@@ -9,25 +9,44 @@ public class sw_YutInfo extends JFrame{
 		Container contentPane = getContentPane();
 		
 		contentPane.setLayout(new BorderLayout(10,10));
+
+		
+		// west 윷판 배치
+		JPanel score = new JPanel();
+		new sw_YutBoardImage(score);
+		contentPane.add(score, BorderLayout.WEST);
+		
 		
 		// center 에는 윷 던진 결과 이미지를 넣는다. 
 		
 		// east 에는 던질 윷 앞/뒤 선택 버튼 
 		JPanel frontback = new JPanel();	// 앞,뒤 버튼이 들어갈 자리를 만든다. 
-		frontback.setLayout(new BorderLayout(4, 20));
+		frontback.setLayout(new BorderLayout(4, 10));
 		JButton front = new JButton("Front");
 		JButton back = new JButton("Back");
 		frontback.add(front, BorderLayout.WEST);
 		frontback.add(back, BorderLayout.EAST);
 		contentPane.add(frontback, BorderLayout.EAST); // 버튼 2개가 들어간 frontback을 east 위치에 추가한다. 
 
-		
 		// south 에는 현재 윷 스택(도개걸윷모 각각)을 보여준다. 
 		JPanel yutstack = new JPanel();
 		new sw_SelectMoveMethod("이동할 칸 수를 고르세요.", yutstack);
-		contentPane.add(yutstack, BorderLayout.SOUTH);
+		frontback.add(yutstack, BorderLayout.SOUTH);
 		
-		setSize(800, 400);
+		
+		JPanel info = new JPanel();
+		info.setLayout(new BorderLayout(4,4));
+		JTextArea systemText = new JTextArea(5, 20);
+		JTextField text = new JTextField();
+		JLabel a = new JLabel("여기에는 점수판이 들어올거임 ");
+		info.add(systemText);
+		info.add(text, BorderLayout.SOUTH);
+		info.add(a, BorderLayout.EAST);
+		
+		contentPane.add(info, BorderLayout.SOUTH);
+
+		
+		setSize(800, score.getHeight()+yutstack.getHeight());
 		setVisible(true); // show frame
 	}
 	
