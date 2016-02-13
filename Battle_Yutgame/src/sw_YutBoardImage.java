4854c77334c86fa8afe6e2d4e854e459d6586005
oberon <over0n91@gmail.com>
2016-02-13 10:34:35 +0900

    윷판 지각버그 패치 테스트

--- Battle_Yutgame/src/sw_YutBoardImage.java
+++ Battle_Yutgame/src/sw_YutBoardImage.java
@@ -12,13 +12,13 @@ public class sw_YutBoardImage extends JFrame {
 		super(title);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500,500);
-		setVisible(true);
 		setResizable(false);
 		
 		ImageIcon image = new ImageIcon("image/YutBoard.jpg");
 		JLabel bg = new JLabel(image);
 		Container c = getContentPane();
 		c.add(bg);
+		setVisible(true);
 		
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(screenSize.width/2-getWidth()/2, screenSize.height/2-getHeight()/2);
