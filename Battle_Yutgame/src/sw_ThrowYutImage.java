4406766ba15eadd3a48f919ac5a9a4bf221d0755
ayCODESabtu <larche0090@gmail.com>
2016-02-13 11:27:45 +0900

    윷던지고 이미지로 출력하는 코드 완성
    씬난다 윷이 던져진다

--- Battle_Yutgame/src/sw_ThrowYutImage.java
+++ Battle_Yutgame/src/sw_ThrowYutImage.java
@@ -1,33 +1,46 @@
 import javax.swing.*;
 
+
 import java.awt.*;
-import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 @SuppressWarnings("serial")
 public class sw_ThrowYutImage extends JFrame{
+	String[] YutName = {"모","도","개","걸","윷"};
+	Random random = new Random();
+	int result; //나중에는 다른곳에서 받아올 변수
+	JLabel[] Yut = new JLabel[4];
+	JLabel YutText = new JLabel("윷을 던져주세요.");
+	ImageIcon front = new ImageIcon("image/Yutfront.png");
+	ImageIcon back = new ImageIcon("image/Yutback.png");
+	JPanel Field = new JPanel();
+	Container cpane = getContentPane(); 
+	
 	sw_ThrowYutImage() {
 		super("윷 던지기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container cpane = getContentPane(); 
 		setSize(700,700);
 		setResizable(false);
 		cpane.setLayout(new BorderLayout(0,50));
-		JPanel Field = new JPanel();
-		Field.setSize(500,400);
+		
+		Field.setSize(500,500);
 		Field.setLayout(null);
 		Field.setBackground(Color.darkGray);
-		try {
-			roll(Field);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		roll();
 		
 		cpane.add(Field,BorderLayout.CENTER);
 		
 		JPanel Text = new JPanel();
-		Text.setLayout(null);
+		Text.setLayout(new FlowLayout());
 		Text.setSize(500,200);
 		Text.setBackground(Color.LIGHT_GRAY);
+		
+		JButton reset = new JButton("다시 던지기");
+		reset.addActionListener(new MyActionListener());
+		Text.add(YutText);
+		Text.add(reset);
 		cpane.add(Text,BorderLayout.SOUTH);
 		
 		setVisible(true);
@@ -35,11 +48,10 @@ public class sw_ThrowYutImage extends JFrame{
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(screenSize.width/2-getWidth()/2, screenSize.height/2-getHeight()/2);
 	}
-	void roll(JPanel Field) throws IOException {
-		ImageIcon front = new ImageIcon("image/Yutfront.png");
-		ImageIcon back = new ImageIcon("image/Yutback.png");
-		int result = (int) (Math.random()*4); //나중에는 다른곳에서 받아올 변수
-		JLabel[] Yut = new JLabel[4];
+	void roll() {
+		result = random.nextInt(4);
+		if(result>2) YutText.setText(YutName[result]+"이 나왔습니다.");
+		else YutText.setText(YutName[result]+"가 나왔습니다.");
 		
 		for(int i=0;i<result;i++) {
 			Yut[i] = new JLabel(front);
@@ -53,13 +65,22 @@ public class sw_ThrowYutImage extends JFrame{
 		}
 		for(int i=0;i<4;i++) //전체 레이블을 랜덤한 위치에 랜덤하게 회전시킴
 		{
-			int x=(int)(Math.random()*(Field.getWidth()));
-			int y=(int)(Math.random()*(Field.getHeight()));
+			int x=random.nextInt(Field.getWidth()-Yut[i].getWidth());
+			int y=random.nextInt(Field.getHeight()-Yut[i].getHeight());
 			Yut[i].setLocation(x, y);
-			//Yut[i].setOpaque(true);
 			Field.add(Yut[i]);
 		}
 	}
+	class MyActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			Field.removeAll();
+			roll();
+			cpane.repaint();
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		new sw_ThrowYutImage();
 	}
