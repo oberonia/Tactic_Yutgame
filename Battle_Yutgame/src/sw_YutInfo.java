e98c4fdf701bd16924623fc362ea4d0707d79b4a
oberon <over0n91@gmail.com>
2016-03-20 01:16:38 +0900

    Mod: Set textfield and textarea public to use at other class.

--- Battle_Yutgame/src/sw_YutInfo.java
+++ Battle_Yutgame/src/sw_YutInfo.java
@@ -6,22 +6,25 @@ import javax.swing.*;
 
 public class sw_YutInfo extends JFrame{
 
+	public JTextField text;
+	public JTextArea systemText;
+	
 	sw_YutInfo() {
 		setTitle("여기는 윷 던지고 결과를 알려줄꺼임");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container contentPane = getContentPane();
-		
+
 		contentPane.setLayout(new BorderLayout(10,10));
 
-		
+
 		// west 윷판 배치
 		JPanel score = new JPanel();
 		new sw_YutBoardImage(score);
 		contentPane.add(score, BorderLayout.WEST);
-		
-		
+
+
 		// center 에는 윷 던진 결과 이미지를 넣는다. 
-		
+
 		// east 에는 던질 윷 앞/뒤 선택 버튼 
 		JPanel frontback = new JPanel();	// 앞,뒤 버튼이 들어갈 자리를 만든다. 
 		frontback.setLayout(new BorderLayout(4, 10));
@@ -35,45 +38,37 @@ public class sw_YutInfo extends JFrame{
 		JPanel yutstack = new JPanel();
 		new sw_SelectMoveMethod("이동할 칸 수를 고르세요.", yutstack);
 		frontback.add(yutstack, BorderLayout.SOUTH);
-		
+
 		// south 에는 점수판과 시스템 텍스트를 보여준다.
 		JPanel info = new JPanel();
 		info.setLayout(new BorderLayout(4,4));
-		final JTextArea systemText = new JTextArea(5, 20);	// 메시지 내역 박스 생성 
+		systemText = new JTextArea(5, 20);	// 메시지 내역 박스 생성 
 		info.add(systemText);	// 화면에 내역 박스 추가 
 		info.add(new JScrollPane(systemText));	// 내역 박스에 스크롤바 추가 
-		
-		JTextField text = new JTextField();	// 메시지 입력 영역 생성 
+
+		text = new JTextField();	// 메시지 입력 영역 생성 
 		info.add(text, BorderLayout.SOUTH);
-		
+
 		text.addActionListener(new ActionListener() {
 			// enter 키를 입력받으면 작동하는 리스너 
 			public void actionPerformed(ActionEvent e) {
 				JTextField t = (JTextField)e.getSource();
 				systemText.append(t.getText()+"\n");	// text에 있는 내용을 systemText에 복사하고 줄바꿈 삽입  
 				t.setText("");	// text를 비움 
-				
+
 			}
 		});
-		
-		
+
+
 		// 점수판을 보여준다.
 		JPanel playerscore = new JPanel();
 		new sw_PlayerInfo(MainFrame.mp, playerscore);
 		info.add(playerscore, BorderLayout.EAST);
-		
+
 		contentPane.add(info, BorderLayout.SOUTH);
 
-		
+
 		setSize(800, score.getHeight()+yutstack.getHeight());
 		setVisible(true); // show frame
 	}
 }
-	
-/*	
-	public static void main(String[] args) {
-		new sw_YutInfo();
-	}
-
-}
-*/
